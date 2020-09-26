c598857fdc3bfd8cd45cf7703846ed25e098c525
DuckSern <nguyendacson2001@gmail.com>
2020-09-27 00:14:35 +0700

    abc

--- src/components/Login/LoginForm.js
+++ src/components/Login/LoginForm.js
@@ -10,11 +10,6 @@ import { withStyles } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
 import { Link } from "react-router-dom";
-import Dialog from "@material-ui/core/Dialog";
-import DialogActions from "@material-ui/core/DialogActions";
-import DialogContent from "@material-ui/core/DialogContent";
-import DialogContentText from "@material-ui/core/DialogContentText";
-import DialogTitle from "@material-ui/core/DialogTitle";
 
 //import Logo
 import logo from "../../images/logo.png";
